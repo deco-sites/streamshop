4c71552c5eba9f10795d3d31bad82c703ea7d910
RyanSantosRibeiro <santosryan131@gmail.com>
2024-12-10 18:05:52 -0300

    direct to app

--- apps/local/streamshop-app.ts
+++ apps/local/streamshop-app.ts
@@ -1,2 +1,2 @@
-export { default } from "../../../Apps/streamshop/streamshop/mod.ts";
-export * from "../../../Apps/streamshop/streamshop/mod.ts";
+export { default } from "https://cdn.jsdelivr.net/gh/deco-cx/apps@6f0613ba5b8bacf69957786584579dd39eb87e2e/streamshop/mod.ts";
+export * from "https://cdn.jsdelivr.net/gh/deco-cx/apps@6f0613ba5b8bacf69957786584579dd39eb87e2e/streamshop/mod.ts";
