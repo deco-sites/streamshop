5350c0f86b267925178dcdbcb6331b556671d3c4
ryansantos <ryan.ribeiro@cadastra.com>
2025-01-03 10:43:53 -0300

    fix app

--- apps/local/streamshop-app.ts
+++ apps/local/streamshop-app.ts
@@ -1,5 +1,6 @@
-export { default } from "https://cdn.jsdelivr.net/gh/deco-cx/apps@7d2498fde30bb4b673e6bf86e65505f951d5510f/streamshop/mod.ts";
-export * from "https://cdn.jsdelivr.net/gh/deco-cx/apps@7d2498fde30bb4b673e6bf86e65505f951d5510f/streamshop/mod.ts";
+// export { default } from "https://cdn.jsdelivr.net/gh/deco-cx/apps@7d2498fde30bb4b673e6bf86e65505f951d5510f/streamshop/mod.ts";
+export { default } from "https://cdn.jsdelivr.net/gh/deco-cx/apps/streamshop/mod.ts";
+export * from "https://cdn.jsdelivr.net/gh/deco-cx/apps/streamshop/mod.ts";
 
 // export { default } from "../../../Apps/streamshop/streamshop/mod.ts";
 // export * from "../../../Apps/streamshop/streamshop/mod.ts";
