f9ca07da65523c79b9b521dfe1bc906ea27d773d
RyanSantosRibeiro <santosryan131@gmail.com>
2025-05-08 13:05:28 -0300

    fix: script

--- apps/local/streamshop-app.ts
+++ apps/local/streamshop-app.ts
@@ -1,6 +1,9 @@
 // export { default } from "https://cdn.jsdelivr.net/gh/deco-cx/apps@7d2498fde30bb4b673e6bf86e65505f951d5510f/streamshop/mod.ts";
-export { default } from "https://cdn.jsdelivr.net/gh/deco-cx/apps/streamshop/mod.ts";
-export * from "https://cdn.jsdelivr.net/gh/deco-cx/apps/streamshop/mod.ts";
+// export { default } from "https://cdn.jsdelivr.net/gh/deco-cx/apps/streamshop/mod.ts";
+// export * from "https://cdn.jsdelivr.net/gh/deco-cx/apps/streamshop/mod.ts";
+
+export { default } from "https://raw.githubusercontent.com/deco-cx/apps/refs/heads/main/streamshop/mod.ts";
+export * from "https://raw.githubusercontent.com/deco-cx/apps/refs/heads/main/streamshop/mod.ts";
 
 // export { default } from "../../../Apps/streamshop/streamshop/mod.ts";
 // export * from "../../../Apps/streamshop/streamshop/mod.ts";
